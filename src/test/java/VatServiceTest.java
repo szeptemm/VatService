1b9dc88a15d87985007ccddc517b566ed44bf96c
Macherzynska <dorota.macherzynska@capgemini.com>
2020-02-11 13:55:25 +0100

    krew i lzy, ale udalo sie! Dzieki, Ola!

--- src/test/java/VatServiceTest.java
+++ src/test/java/VatServiceTest.java
@@ -5,6 +5,8 @@ import org.mockito.Mockito;
 
 import java.math.BigDecimal;
 import java.util.UUID;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -26,5 +28,32 @@ class VatServiceTest {
         //then
 
         assertThat(grossPrice).isEqualTo(new BigDecimal("21.53"));
+    }
+
+    @Test
+    void shouldCalculateGrossPriceForBooks() {
+        //given
+        VatProvider vatProvider = Mockito.mock(VatProvider.class);
+        Mockito.when(vatProvider.getVatFor(anyString(),any(Type.class) )).thenReturn(new BigDecimal("0.10"));
+        VatService serviceToTest = new VatService(vatProvider);
+        //when
+        Stream<BigDecimal> grossPrices = serviceToTest.getGrossPrices(generateStreamOfProducts());
+
+        BigDecimal sum = grossPrices
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+        //then
+        assertThat(sum).isEqualTo(new BigDecimal("252.45"));
+    }
+
+    private Stream<Product> generateStreamOfProducts() {
+
+        Stream<Product> product =
+                IntStream.range(1,10)
+                .mapToObj(index -> {
+                    BigDecimal price = BigDecimal.valueOf(index).add(new BigDecimal("20.50"));
+                    return new Product(UUID.randomUUID(), "My book", price, Type.BOOK, "Poland");
+                });
+
+        return product;
     }
 }
